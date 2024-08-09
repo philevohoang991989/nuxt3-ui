7fe0a92e714349202753dbe5a0c0d93f6f44e341
DESKTOP-IPD4HCO\phi.le <phi.le@bnksolution.com>
2024-08-09 11:36:43 +0700

    init code

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Test</h1>
+    <UButton
+      :loading="loading"
+      label="Button"
+      size="sm"
+    />
+  </div>
+</template>
+
+<script setup>
+let loading = ref(true);
+</script>
+
+<style lang="scss" scoped></style>
